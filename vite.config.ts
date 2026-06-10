840e8dceb70b7f053045c3091fa322587f03a56b
Jari Tynkkynen <jari.tynkkynen@elisanet.fi>
2026-06-10 17:22:05 +0300

    doc folder

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/PersonalBrand/',
   plugins: [react()],
   assetsInclude: ['**/*.pdf'],
   server: {
